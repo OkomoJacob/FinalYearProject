a3da6c880c4183182a5a402c733fa3446580bb20
okomojacob <jaylabs2030@gmail.com>
2021-09-20 21:52:16 +0300

    :hammer:, removed, :fire:

--- ModelsAlgorithmsCodes/4.R Codes/1.chl-g.R
+++ ModelsAlgorithmsCodes/4.R Codes/1.chl-g.R
@@ -4,21 +4,25 @@ library(raster)
 library(rgdal)
 
 # import default proj dir and Check path contents
-path <- setwd("D:/STUDY/4.GIS/G I S 4.2/0.Project/RESULTS/Optical ERDAS")
+path <- setwd("D:/STUDY/4.GIS/G I S 4.2/0.Project/RESULTS/opticalRasters")
 path
 contents <- length(list.files(path))
 contents
 
 # Denote as Multiraster data
-
-aoiRaster <- stack()"L8NgaraAOI/NgaraAOI.tif"
+aoiRaster <- stack("2020_06_09_Lyr_Clip.tif")
 aoiRaster
 
-# Aalgorithm starts here
-plotRGB(aoiRaster, 4,3,2, scale = 65535, stretch = 'lin')
-ndvi <- (aoiRaster[[5]]-aoiRaster[[4]])/(aoiRaster[[5]]+aoiRaster[[4]])
-ndvi
-plot(ndvi,  main = "NGARA NDVI RAW")
+# Algorithm starts here
+# Chl-a=10^((0.2511-2.0853R+1.5035R^2-3.1747R^3+0.3383R^4))
+# Where R=log_10⁡〖Rrs(490)⁄Rrs(555) 〗
+# 490 = Blue, 555 = Green
+# ndvi <- (aoiRaster[[5]]-aoiRaster[[4]])/(aoiRaster[[5]]+aoiRaster[[4]])
+R = log10(Blue/Green)
 
+Chl_a = 10^((0.2511-2.0853*R + 1.5035*R^2-3.1747*R^3+0.3383*R^4))
+plotRGB(aoiRaster, 4,3,2, scale = 65535, stretch = 'lin')
+plot(Chl_a,  main = "06_09_2020")
+aoiRaster[16]
 # Save file on disk
-saveFile <- writeRaster(ndvi, "ngaraNDVI.tiff", format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
+# saveFile <- writeRaster(ndvi, "ngaraNDVI.tiff", format = "GTiff", datatype = "FLT4S", overwrite = TRUE)
