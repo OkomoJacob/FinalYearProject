6e481cc2c3ab846f36f311d78d38b73d25df3c45
okomojacob <jaylabs2030@gmail.com>
2021-10-18 05:53:10 +0300

    Works well but we have to customize the plots

--- ModelsAlgorithmsCodes/6.Accurcay Assesment/LinearRegression.R
+++ ModelsAlgorithmsCodes/6.Accurcay Assesment/LinearRegression.R
@@ -1,5 +1,6 @@
 # Clear memory
 rm(list = ls(all=TRUE))
+library(ggplot2)
 
 # import default proj dir and Check path contents
 path <- setwd("D:/STUDY/4.GIS/G I S 4.2/0.Project/ModelsAlgorithmsCodes/6.Accurcay Assesment")
@@ -7,7 +8,7 @@ path
 contents <- length(list.files(path))
 contents
 
-data <- read.csv("Correlation2015.csv")
+data <- read.csv("Correlation2018.csv")
 summary(data)
 plot(data$L8.Chl.a.Estimates, data$S3.OLCI.Chl.a..Reference.)
 
@@ -18,10 +19,26 @@ plot(data$L8.Chl.a.Estimates, data$S3.OLCI.Chl.a..Reference.)
 randomNums1 = runif(60)
 
 # Index the radon numbers
-shuffle <- order(randomNums1)
+indexedData <- order(randomNums1)
 
 # Create training dataset with the first 20 records in the shuffle
-training <- data[shuffle[1:30],]
+training <- data[indexedData[1:45],]
 
 # Test dataset
-test_ds = data[shulffle[21:30]]
+test_ds = data[indexedData[46:60],]
+
+# Build the prediction linear model
+regressor <- lm(S3.OLCI.Chl.a..Reference.~L8.Chl.a.Estimates, data = training)
+## abline plot
+abline(regressor, col = "red")
+
+## Model summary
+summary(regressor)
+
+# Regression line
+# y = 6.05839 + 0.074977(l8)
+
+## Plotting predictions of the values
+# plot(regressor)
+
+## Customize your plots from here using ggplot2
