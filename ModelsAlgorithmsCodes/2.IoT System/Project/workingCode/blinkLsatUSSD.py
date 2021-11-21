9cd8059bffa9e6249f9db0e7260cb95089c9f1b4
okomojacob <jaylabs2030@gmail.com>
2021-11-21 18:01:43 +0300

    :necktie: Recieves data and blnks an LED, then sends text SMS :fire:, :sparkles:

---
+++ ModelsAlgorithmsCodes/2.IoT System/Project/workingCode/blinkLsatUSSD.py
@@ -0,0 +1,58 @@
+'''Collects Air Temperauture via DHT11 and blinks LED, send SMS notification if Temp goes above threshold'''
+import africastalking as at
+import RPi.GPIO as GPIO
+import datetime
+import dht11
+import time #To delay your LED, LSAT collection timing
+
+#Initialize Africastalking
+sms_username = "myTrial"
+sms_api_key = "c667670a2a2b2df9812f5421405d1dff0ba12544b502548eef5b2c5b147e5042"
+at.initialize(sms_username,sms_api_key)
+recepients = recipients = ['+254705583483']
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+ledPin = 4 #Use pin 7 (GPIO 4)
+GPIO.setup(ledPin, GPIO.OUT)
+
+# read dht11 data using GPIO pin 17
+instance = dht11.DHT11(pin=17)
+# The Real Guy
+print("Connection Secured!")
+time.sleep(2.5)
+print("Connecting to GPS...")
+time.sleep(6)
+print("LSAT collection start...")
+#Flashing
+#while True: #Run forever
+
+# main meat execution    
+try:
+    while True:
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(0.25)
+        #  print("Going Off")
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.25)
+        
+        result = instance.read()
+        lsat = result.temperature
+        if result.is_valid():
+            print("Last valid input: " + str(datetime.datetime.now()))
+            print("LSAT: %-3.1f °C\n" % lsat)
+            
+        def send_sms():
+            sms = at.SMS
+            response = sms.send("Tm f'{lsat} C", recepients)
+            print(response)
+            
+        # main meat execution    
+        if lsat >=30:
+            send_sms()
+            
+except KeyboardInterrupt:
+    print("Cleanup")
+    GPIO.cleanup()
