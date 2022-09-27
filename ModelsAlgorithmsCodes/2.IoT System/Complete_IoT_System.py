0b32be3a0717401e6de9c9f8efe1d01f9f64ca29
okomojacob <okomo.code@gmail.com>
2022-09-27 16:48:33 +0300

    Complete Styem as at 2022

---
+++ ModelsAlgorithmsCodes/2.IoT System/Complete_IoT_System.py
@@ -0,0 +1,123 @@
+import time
+import random
+import requests
+import africastalking
+import Adafruit_DHT
+import serial
+import pynmea2
+from datetime import datetime
+
+# Setup Africa's Talking API setup
+AT_USERNAME = "lsat_username"  
+AT_API_KEY = "lsat_api_key"  
+africastalking.initialize(AT_USERNAME, AT_API_KEY)
+sms = africastalking.SMS
+
+# Cloud server URL (mock for demonstration)
+CLOUD_SERVER_URL = "https://lsat.sampleserver.com/api/v1" #This was brought down due to monetary mainataince costs it attracted.
+
+# Sensor setup
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4  # GPIO pin for DHT11
+GPS_PORT = "/dev/ttyS0"  # Serial port for GPS (Neo-6M)
+
+# Threshold for LSAT to trigger SMS (based on HAB conditions)
+LSAT_THRESHOLD = 28.0  # Temp in °C (This is adjusted based on the study areas LSAT Thresholds)
+
+# Simulate salinity sensor (This was removed due to lack of the sensor during study, recommendations were made.)
+def get_salinity():
+    return "NOT_SET"
+    
+# Simulate system power level and water condition
+def get_system_status():
+    try:
+        with open('/sys/class/power_supply/battery/voltage_now', 'r') as file:
+            voltage = float(file.read().strip()) / 1000000  # Convert microvolts to volts
+        max_voltage = 4.2  # Max for LiPo battery
+        min_voltage = 3.3  # MIn for Raspberry Pi
+        percentage = ((voltage - min_voltage) / (max_voltage - min_voltage)) * 100
+        return max(0, min(100, round(percentage, 2)))
+    except Exception:
+        return None
+
+
+# Get LSAT using DHT11
+def get_lsat():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    if temperature is not None:
+        return temperature
+    return 0.0  # Fallback if reading fails
+
+# Get GPS coordinates using Neo-6M
+def get_gps_location():
+    try:
+        ser = serial.Serial(GPS_PORT, 9600, timeout=1)
+        for _ in range(10):  # Try 10 times to get a valid GPS fix
+            line = ser.readline().decode('utf-8').strip()
+            if line.startswith('$GPGGA'):
+                msg = pynmea2.parse(line)
+                if msg.lat and msg.lon:
+                    lat = float(msg.lat) / 100  # Convert to decimal degrees
+                    lon = float(msg.lon) / 100
+                    return lat, lon
+    except Exception as e:
+        print(f"GPS Error: {e}")
+    return -1.059932, 37.01833  # Default fallback coordinates.
+
+# Send data to cloud server
+def send_to_cloud(data):
+    try:
+        response = requests.post(CLOUD_SERVER_URL, json=data)
+        if response.status_code == 200:
+            print("Sensor data sent to cloud successfully!")
+            return True
+        else:
+            print(f"Failed to send data: {response.status_code}")
+    except Exception as e:
+        print(f"Cloud Transmission Error: {e}")
+    return False
+
+# Send SMS via Africa's Talking API
+def send_sms_alert(message):
+    try:
+        recipients = ["+254 (0)-12-3456-789"]  # Replace with Maritime Authority phone number
+        response = sms.send(message, recipients)
+        print(f"SMS Sent: {response}")
+    except Exception as e:
+        print(f"SMS Error: {e}")
+
+# Main data collection and transmission loop
+def main():
+    while True:
+        # Collect data
+        lsat = get_lsat()
+        salinity = get_salinity()
+        lat, lon = get_gps_location()
+        power_level, water_condition = get_system_status()
+        timestamp = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S GMT")
+
+        # Prepare data for transmission
+        data = {
+            "lsat": lsat,
+            "salinity": salinity,
+            "location": {"lat": lat, "lon": lon},
+            "power_level": power_level,
+            "water_condition": water_condition,
+            "timestamp": timestamp
+        }
+
+        # Send to cloud
+        if send_to_cloud(data):
+            # Check if LSAT exceeds threshold for HAB alert
+            if lsat > LSAT_THRESHOLD:
+                message = (f"LSAT abnormally detected! Temp: {lsat:.1f} °C, Lake Surf. Air, "
+                          f"Water Salinity: {salinity}, Sensors Location: {lat:.6f}, {lon:.6f}, "
+                          f"View Map: https://www.google.com/maps?q={lat:.6f},{lon:.6f}, "
+                          f"System Power Level: {power_level}%, Water conditions: {water_condition}")
+                send_sms_alert(message)
+
+        # Wait before next sample (e.g., every 60 seconds)
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
--- ModelsAlgorithmsCodes/2.IoT System/README.md
+++ ModelsAlgorithmsCodes/2.IoT System/README.md
@@ -1,9 +1,107 @@
-## System Authentication
-username: pi <br>
-IP Address: 192.168.43.220 <br>
-Password: spac*5 <br>
+# LSAT IoT Monitoring System
+## Overview
 
-## Problems
-1. [Raspi Green LED Blinks 4 times then goes off](https://forums.raspberrypi.com/viewtopic.php?t=272263)
-2. [Forum,Boot problems listed](https://forums.raspberrypi.com/viewtopic.php?f=28&t=58151)
-3. [Pins Explained](https://www.raspberrypi.com/documentation/computers/os.html#gpio-and-the-40-pin-header)
+This project implements a real-time environmental monitoring system designed to detect Harmful Algal Blooms (HABs) in lake ecosystems. It collects data on Lake Surface Air Temperature (LSAT), salinity, GPS location, system power level and water condition, transmitting this data to a cloud server and sending SMS alerts when LSAT exceeds a predefined threshold.
+
+### Features
+ -[X] `Temperature Monitoring:` Measures LSAT using a DHT11 sensor.
+ -[X] `GPS Tracking:` Captures location data with a Neo-6M GPS module.
+ -[X] `Cloud Integration:` Sends data to a cloud server via HTTP POST requests.
+ -[X] `SMS Alerts:` Uses Africa's Talking API to send alerts when LSAT indicates potential HAB conditions.
+ -[X] `System Status:` Monitors power level and water condition with simulated values.
+
+## Hardware Requirements
+
+ -[X] Raspberry Pi Model 3 B+
+ -[X] DHT11 Sensor: For temperature and humidity measurements (connected to GPIO pin 4).
+ -[X] Neo-6M GPS Module: For location data (connected to serial port /dev/ttyS0).
+ -[X] Internet Connection: For cloud data transmission and SMS alerts.
+ -[X] Power Supply: Stable power source for continuous operation.
+
+## Software Requirements
+
+Python 3.2+
+Libraries:
+
+`Adafruit_DHT:` For DHT11 sensor interfacing.
+`pynmea2:` For parsing GPS NMEA data.
+`requests:` For HTTP requests to the cloud server.
+`africastalking:` For SMS alerts via Africa's Talking API.
+`pyserial:` For serial communication with the GPS module.
+
+
+Operating System: Linux-based (e.g., Raspberry Pi OS).
+
+## Installation
+
+Clone the Repository:
+git clone https://github.com/OkomoJacob/FinalYearProject.git
+Navigate to this directory and locate `Complete_IoT_System.py` file
+
+
+## Install Dependencies:
+```shell
+$ pip install -r requirements.txt
+```
+
+### Set Up Africa's Talking API:
+
+Sign up at Africa's Talking & obtain your AT_USERNAME and AT_API_KEY.
+Update the script with these credentials: AT_USERNAME = "lsat_username"
+AT_API_KEY = "lsat_api_key"
+
+
+### Hardware Configuration.
+
+- Connect the DHT11 sensor to GPIO pin 4.
+- Connect the Neo-6M GPS module to the serial port /dev/ttyS0.
+
+Run the Script:
+On power on, the system is configured to automatically start data transmission
+
+### Configuration
+
+### Usage
+
+Once powered, the system runs continuously, collecting data every 3600 seconds (1 Hour or as desired).
+Data is sent to the configured cloud server.
+If LSAT exceeds the threshold, an SMS alert is sent with temperature, salinity, location, and system status details, including a Google Maps link.
+
+Data Format
+The system sends JSON data to the cloud server in the following format:
+{
+  "lsat": 25.5,
+  "salinity": "NOT_SET",
+  "location": {
+    "lat": -1.059932,
+    "lon": 37.01833
+  },
+  "power_level": 85,
+  "water_condition": "OK",
+  "timestamp": "2025-05-07 12:34:56 GMT"
+}
+
+Limitations
+
+Salinity Sensor: Currently a placeholder (NOT_SET). Integrate a real salinity sensor for complete functionality.
+Mock Cloud Server: The CLOUD_SERVER_URL is a placeholder. Replace with a real endpoint.
+Power Level Simulation: Power level and water condition are simulated. Integrate actual sensors for production use.
+Error Handling: Basic error handling is implemented; enhance for robustness in production environments.
+
+Contributing
+Contributions are welcome! Please:
+
+Fork the repository.
+Create a feature branch (git checkout -b feature/your-feature).
+Commit your changes (git commit -m "Add your feature").
+Push to the branch (git push origin feature/your-feature).
+Open a pull request.
+
+License
+This project is licensed under the MIT License. See the LICENSE file for details.
+Acknowledgments
+
+Africa's Talking for SMS API services.
+Adafruit for DHT sensor libraries.
+The environmental science community for HAB research informing this project.
+
